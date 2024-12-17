9015e4357917df81d19d69c79e01439bccf5fd7e
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2024-12-17 11:38:47 +0300

    backend/namma-dsl: regenerated all code

--- Backend/app/rider-platform/rider-app/Main/src-read-only/Storage/Queries/OrphanInstances/WalkLegMultimodal.hs
+++ Backend/app/rider-platform/rider-app/Main/src-read-only/Storage/Queries/OrphanInstances/WalkLegMultimodal.hs
@@ -22,7 +22,7 @@ instance FromTType' Beam.WalkLegMultimodal Domain.Types.WalkLegMultimodal.WalkLe
     pure $
       Just
         Domain.Types.WalkLegMultimodal.WalkLegMultimodal
-          { estimatedDistance = (Kernel.Types.Common.Distance estimatedDistance distanceUnit),
+          { estimatedDistance = Kernel.Types.Common.Distance estimatedDistance distanceUnit,
             estimatedDuration = estimatedDuration,
             fromLocation = fromLocation',
             id = Kernel.Types.Id.Id id,
@@ -39,20 +39,20 @@ instance FromTType' Beam.WalkLegMultimodal Domain.Types.WalkLegMultimodal.WalkLe
 instance ToTType' Beam.WalkLegMultimodal Domain.Types.WalkLegMultimodal.WalkLegMultimodal where
   toTType' (Domain.Types.WalkLegMultimodal.WalkLegMultimodal {..}) = do
     Beam.WalkLegMultimodalT
-      { Beam.distanceUnit = ((.unit)) estimatedDistance,
-        Beam.estimatedDistance = ((.value)) estimatedDistance,
+      { Beam.distanceUnit = (.unit) estimatedDistance,
+        Beam.estimatedDistance = (.value) estimatedDistance,
         Beam.estimatedDuration = estimatedDuration,
-        Beam.fromLocationId = (Just $ Kernel.Types.Id.getId ((.id) fromLocation)),
+        Beam.fromLocationId = Just $ Kernel.Types.Id.getId ((.id) fromLocation),
         Beam.id = Kernel.Types.Id.getId id,
-        Beam.agency = (journeyLegInfo >>= (.agency)),
+        Beam.agency = journeyLegInfo >>= (.agency),
         Beam.convenienceCost = Kernel.Prelude.fmap (.convenienceCost) journeyLegInfo,
         Beam.journeyId = Kernel.Prelude.fmap (.journeyId) journeyLegInfo,
         Beam.journeyLegOrder = Kernel.Prelude.fmap (.journeyLegOrder) journeyLegInfo,
-        Beam.pricingId = (journeyLegInfo >>= (.pricingId)),
+        Beam.pricingId = journeyLegInfo >>= (.pricingId),
         Beam.skipBooking = Kernel.Prelude.fmap (.skipBooking) journeyLegInfo,
         Beam.riderId = Kernel.Types.Id.getId riderId,
         Beam.startTime = startTime,
-        Beam.toLocationId = (Kernel.Types.Id.getId <$> (toLocation <&> (.id))),
+        Beam.toLocationId = Kernel.Types.Id.getId <$> (toLocation <&> (.id)),
         Beam.merchantId = Kernel.Types.Id.getId <$> merchantId,
         Beam.merchantOperatingCityId = Kernel.Types.Id.getId <$> merchantOperatingCityId,
         Beam.createdAt = createdAt,
